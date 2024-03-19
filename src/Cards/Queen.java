122e7aa045b46371b0d9b72720d865fc8c6b5eba
CloudyFroggo <68325474+CloudyFroggo@users.noreply.github.com>
2024-03-19 19:26:53 +0800

    Added back Queen's move logic

--- src/Cards/Queen.java
+++ src/Cards/Queen.java
@@ -1,5 +1,7 @@
 package Cards;
 
+import Locations.Location;
+
 public class Queen extends Card {
     private boolean canMove;
 
@@ -11,6 +13,31 @@ public class Queen extends Card {
     @Override
     public boolean canMove() {
         return this.canMove;
+    }
+
+    public void move(Location oldLocation, Location newLocation, boolean p1) throws Exception {
+        if (!canMove) {
+            throw new Exception("Queen has already moved this game");
+        }
+
+        if (oldLocation.equals(newLocation)) {
+            throw new Exception("You're attempting to move him to the same place!");
+        }
+
+        int indexOfQueen = -1;
+
+        for (int i = 0; i < oldLocation.getCards(p1).size(); i++) {
+            if (this.equals(oldLocation.getCards(p1).get(i))) {
+                indexOfQueen = i;
+            }
+        }
+
+        if (indexOfQueen == -1) {
+            throw new Exception("Error! queen can't be found");
+        }
+
+        newLocation.placeCard(oldLocation.removeCard(indexOfQueen, p1), p1);
+        this.canMove = false;
     }
 
     public void hasMoved() {
