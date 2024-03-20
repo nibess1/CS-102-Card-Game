e3045b77211fe44a075ef205bc455c3408e57919
kimshanon <ksa.ganap.2023@smu.edu.sg>
2024-03-20 23:24:22 +0800

    Update CIS

--- src/Locations/CIS.java
+++ src/Locations/CIS.java
@@ -1,8 +1,37 @@
 package Locations;
+
 import Cards.*;
 import Exception.*;
 import java.util.*;
 
-public class CIS extends Location{
-    
+public class CIS extends Location {
+
+    private ArrayList<Card> p1LiveCards;
+    private ArrayList<Card> p2LiveCards;
+
+    public CIS() {
+        super.setName("CIS");
+        super.setDescription("Only picture cards can be placed here. Their powers will be neutralised.");
+    }
+
+    @Override
+    public void placeCard(Card cardToBePlaced, boolean p1) {
+
+        if (!Card.isPictureCard(cardToBePlaced)) {
+            throw new LocationRejectionException("This card is not a picture card.");
+        }
+
+        if (cardToBePlaced instanceof Queen queenCard) {
+            queenCard.setCanMove(false);
+        }
+
+        if (p1) {
+            this.p1LiveCards.add(cardToBePlaced);
+        } else {
+            this.p2LiveCards.add(cardToBePlaced);
+        }
+
+        calculatePower(p1);
+    }
+
 }
