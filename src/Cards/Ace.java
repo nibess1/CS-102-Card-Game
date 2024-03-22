9a878e72f35a3a43e6bf901a1dcaf199f8d6d90e
CloudyFroggo <68325474+CloudyFroggo@users.noreply.github.com>
2024-03-22 08:26:09 +0800

    Fixed Ace on Ace loop

--- src/Cards/Ace.java
+++ src/Cards/Ace.java
@@ -3,7 +3,7 @@ package Cards;
 import Locations.Location;
 
 public class Ace extends Card implements Picture {
-    public static final String ability = "Activate your cards with abilities once more";
+    public static final String ability = "Activate your picture abilities (Except Ace) once more.";
 
     public Ace(char suite) {
         super(1, suite);
@@ -14,6 +14,10 @@ public class Ace extends Card implements Picture {
             if (card instanceof Picture) {
                 if (card instanceof Queen q) {
                     q.setCanMove(true);
+                }
+                // if there's already an ace there, don't trigger ace.
+                else if (card instanceof Ace) {
+                    continue;
                 } else {
                     // remove and place the card again to retrigger effects
                     location.removeCard(card, p1);
